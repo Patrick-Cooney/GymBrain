58673b12b6aa9cc4ed79257b6042fdb636dbc311
Patrick Cooney <patrick@robotsintre.es>
2013-08-10 20:43:38 -0500

    Initial Commit

---
+++ GymBrain/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GymBrain
+//
+//  Created by Patrick Cooney on 8/10/13.
+//  Copyright (c) 2013 Patrick Cooney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
